94bd7126ce7ae7a4e214499bd020cbb83a500cba
Romain Dauby <romain.dauby@gmail.com>
2014-03-07 09:58:01 +0100

    Refacto du code + autoloader

---
+++ controller/documentation.php
@@ -0,0 +1,26 @@
+<?php
+	function afficherDoc(){		
+		echo '<thead><tr><th>No</th><th>Question</th><th>Non</th><th>Oui</th></tr></thead><tbody>';
+		$enregistrements = Db::queryObject('SELECT * FROM arbre');
+		foreach ($enregistrements as $enregistrement) {
+			echo '<tr>';
+			afficherInfos($enregistrement);
+			echo '</tr>';
+		}
+		echo '</tbody>';
+	}
+
+	function afficherInfos($ereg){
+		echo '<td>'.$ereg->id.'</td>';
+		echo '<td>'.$ereg->question.' ?</td>';
+		afficherSuivant($ereg->s1);
+		afficherSuivant($ereg->s2);
+	}
+	
+	function afficherSuivant($next){
+		if($next != 0){
+			$suivant = Db::querySingle('SELECT question from arbre WHERE id='.$next);
+			echo '<td>'.$suivant->question.' ?</td>';
+		}else echo '<td><span class="glyphicon glyphicon-ok"></span> <span style="color:green;">On a une solution !</span></td>'; 
+	}
+?>
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-    require_once('documentation.php');
-    require_once('settings.php');
+    require_once('autoload.php');
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -11,34 +10,34 @@
     </head>
     <body>
     	<div class="container">
-    		<div class="row">
-    			<div class="col-md-10">
-                    <h1> Projet concernant la création d'un système expert</h1>
-                    <h4><u> Etudiant participant au projet : </u></h4>
-                        <p>
-                            <ul>
-                                <li> Romain DAUBY (chef de projet) </li>
-                                <li> Thomas LORENZATO </li>
-                                <li> Loïc MERCIER </li>
-                                <li> Vincent FADDA </li>
-                                <li> Pierrick NANOT </li>
-                                <li> Félix WATTEZ </li>
-                            </ul>
-                        </p>
-                    </h4>  
-    				<h2>Informations nécessaires pour la documentation</h2>
-                    <table id="res" class="table table-striped table-bordered table-hover">
-                        <?=getDoc()?>
-                    <table>
-    			</div>
-    		</div>
+	    <div class="row">
+		<div class="col-md-10">
+		    <h1> Projet concernant la création d'un système expert</h1>
+		    <h4><u> Etudiant participant au projet : </u></h4>
+			<p>
+			    <ul>
+				<li> Romain DAUBY (chef de projet) </li>
+				<li> Thomas LORENZATO </li>
+				<li> Loïc MERCIER </li>
+				<li> Vincent FADDA </li>
+				<li> Pierrick NANOT </li>
+				<li> Félix WATTEZ </li>
+			    </ul>
+			</p>
+		    </h4>  
+		    <h2>Informations nécessaires pour la documentation</h2>
+		    <table id="res" class="table table-striped table-bordered table-hover">
+			<?=afficherDoc()?>
+		    <table>
+		</div>
+	    </div>
     	</div>
-		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-		<script src="js/jquery.dataTables.min.js"></script>
-		<script>
-			$(document).ready(function(){
-				$('#res').dataTable();
-			});
-		</script>
-	</body>
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script>
+	    $(document).ready(function(){
+		$('#res').dataTable();
+	    });
+	</script>
+    </body>
 </html>
