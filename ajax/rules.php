5b862d68b40446f6753593935d7c8d036a2abad6
thomasgheberg <thomas.lorenzato@orange.fr>
2014-05-31 17:57:15 +0200

    Mise au format json #2

--- ajax/rules.php
+++ ajax/rules.php
@@ -1,12 +1,24 @@
 <?php
-require_once("autoload.php");
+require_once("../autoload.php");
 
 $text="[";
 $results=Db::queryObject("select goal,facts from rules order by id");
 foreach($results as $result)
 {
-	$text.='{goal: '.$result->goal.', facts: '.$result->facts.'},';
+	$resultGoal=Db::querySingle("select libelle from fact where id=$result->goal");	
+	$text.='{id: '.$result->goal.', goal: '.$resultGoal->libelle.', facts: [';
+
+	$factExplode=explode(",", $result->facts);
+	foreach($factExplode as $id)
+	{
+		$resultFacts=Db::querySingle("select libelle from fact where id=$id");
+		 $text.='{id: '.$id.', text: "'.$resultFacts->libelle.'"},';
+	}
+
+	$text=substr($text, 0, -1);
+	$text.=']},';
 }
+$text=substr($text, 0, -1);
 $text.="]";
 
 echo $text;
