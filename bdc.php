99784af11741f4de65ddcace48e01ec34bbe0ea6
Romain Dauby <romain.dauby@gmail.com>
2014-06-21 12:20:56 +0200

    Liste des questions /bdc.php

--- bdc.php
+++ bdc.php
@@ -12,32 +12,46 @@
     	<div class="container">
             <div class="row">
                 <div class="col-md-10">
-                    <h1> Projet concernant la création d'un système expert</h1>
-                    <h4><u> Etudiant participant au projet : </u></h4>
-                    <p>
-                        <ul>
-                        <li> Romain DAUBY (chef de projet) </li>
-                        <li> Thomas LORENZATO </li>
-                        <li> Loïc MERCIER </li>
-                        <li> Vincent FADDA </li>
-                        <li> Pierrick NANOT </li>
-                        <li> Félix WATTEZ </li>
-                        </ul>
-                    </p>
-                    </h4>  
-                    <h2>Base de connaissance</h2>
-                    <h3>Liste des faits</h3><hr>
-                    <table id="res" class="table table-striped table-bordered table-hover">
-                    <?=afficherBdc()?>
-                    <table>
+                    <div class="row">
+                        <h1> Projet concernant la création d'un système expert</h1>
+                        <h4><u> Etudiant participant au projet : </u></h4>
+                        <p>
+                            <ul>
+                            <li> Romain DAUBY (chef de projet) </li>
+                            <li> Thomas LORENZATO </li>
+                            <li> Loïc MERCIER </li>
+                            <li> Vincent FADDA </li>
+                            <li> Pierrick NANOT </li>
+                            <li> Félix WATTEZ </li>
+                            </ul>
+                        </p>
+                        </h4>
+                    </div>
+                    <div class="row">
+                        <h2>Base de connaissance</h2>
+                    </div>
+                    <div class="row">
+                        <h3>Liste des faits</h3><hr>
+                        <table id="faits" class="table table-striped table-bordered table-hover">
+                        <?=afficherFaits()?>
+                        </table>
+                    </div>
+                    <div class="row">
+                        <h3>Liste des questions</h3><hr>
+                        <table id="questions" class="table table-striped table-bordered table-hover">
+                        <?=afficherQuestions()?>
+                        </table>
+                    </div>
                 </div>
             </div>
     	</div>
 	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<script src="js/lib/jquery.dataTables.min.js"></script>
+    <script src="js/lib/datatables.bootstrap.js"></script>    
 	<script>
 	    $(document).ready(function(){
-            $('#res').dataTable();
+            $('#faits').dataTable();
+            $('#questions').dataTable();
 	    });
 	</script>
     </body>
--- model/documentation.php
+++ model/documentation.php
@@ -1,11 +1,23 @@
 <?php
-	function afficherBdc(){
+	function afficherFaits(){
 		echo '<thead><tr><th>Numero</th><th>Libelle</th></thead><tbody>';
 		$enregistrements = Db::queryObject('SELECT id, fait FROM fait');
 		foreach ($enregistrements as $enregistrement) {
 			echo '<tr>';
 			echo '<td>'.$enregistrement->id.'</td>';
 			echo '<td>'.$enregistrement->fait.'</td>';
+			echo '</tr>';
+		}
+		echo '</tbody>';
+	}
+	
+	function afficherQuestions(){
+		echo '<thead><tr><th>Numero</th><th>Libelle</th></thead><tbody>';
+		$enregistrements = Db::queryObject('SELECT id, but FROM regle WHERE faits_suivants <> \'\'');
+		foreach ($enregistrements as $enregistrement) {
+			echo '<tr>';
+			echo '<td>'.$enregistrement->id.'</td>';
+			echo '<td>'.$enregistrement->but.'</td>';
 			echo '</tr>';
 		}
 		echo '</tbody>';
